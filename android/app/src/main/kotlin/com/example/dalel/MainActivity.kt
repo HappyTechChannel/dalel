c866876109471305ac44d166d9c10f6d49118dd4
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-09-09 00:25:20 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/dalel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dalel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Dalel());
+}
+class Dalel extends StatelessWidget {
+  const Dalel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
