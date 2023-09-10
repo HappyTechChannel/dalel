544cfc4fd698092a6f3a2f244ca7cbdb2c05f329
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-09-10 08:49:55 +0300

    App Assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,31 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesForgotPassword
+  /// assets/images/forgotPassword.png
+  static const String imagesForgotPassword = "assets/images/forgotPassword.png";
+
+  /// Assets for imagesOnBoarding
+  /// assets/images/onBoarding.png
+  static const String imagesOnBoarding = "assets/images/onBoarding.png";
+
+  /// Assets for imagesOnBoarding1
+  /// assets/images/onBoarding1.png
+  static const String imagesOnBoarding1 = "assets/images/onBoarding1.png";
+
+  /// Assets for imagesOnBoarding2
+  /// assets/images/onBoarding2.png
+  static const String imagesOnBoarding2 = "assets/images/onBoarding2.png";
+
+  /// Assets for imagesSucccesOrder
+  /// assets/images/succcesOrder.png
+  static const String imagesSucccesOrder = "assets/images/succcesOrder.png";
+
+  /// Assets for imagesVector1
+  /// assets/images/vector1.svg
+  static const String imagesVector1 = "assets/images/vector1.svg";
+
+  /// Assets for imagesVector2
+  /// assets/images/vector2.svg
+  static const String imagesVector2 = "assets/images/vector2.svg";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:dalel/core/utils/app_assets.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const Dalel());
 }
+
 class Dalel extends StatelessWidget {
   const Dalel({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Image.asset(Assets.imagesOnBoarding2),
+        ),
+      ),
+    );
   }
 }
