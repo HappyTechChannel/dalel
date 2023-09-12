56dc363e577b3536d0845c1c7ec333bd6fd699b5
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-09-13 00:51:53 +0300

    App Strings

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,69 @@
+abstract class AppStrings {
+  //ToDo:All Strings
+
+  static const String appName = "Dalel",
+      //!On Boarding Strings
+      skip = "Skip",
+      next = "Next",
+      createAccount = "Create Account",
+      loginNow = "Login Now",
+      //!Auth Strings
+      welcome = "Welcome!",
+      welcomeBack = "Welcome Back!",
+      fristName = "First Name",
+      lastName = "Last Name",
+      emailAddress = "Email Address",
+      password = "Password",
+      iHaveAgreeToOurTermsAndCondition =
+          "I have agree to our Terms and Condition",
+      signUp = "Sign Up",
+      signIn = "Sign In",
+      alreadyHaveAnAccount = "Already have an account ?",
+      dontHaveAnAccount = "Don’t have an account ?",
+      forgotPassword = "Forgot Password ?",
+      //!Home Strings
+      recommendations = "Recommendations",
+      aboutAncientEgypt = "About Ancient Egypt",
+      ancientEgyptWars = "Ancient Egypt Wars",
+      //!Search Strings
+      searchDeepIntoTheHistory = "Search deep into the history",
+      recent = "Recent",
+      //!Profile Strings
+      profile = "Profile",
+      account = "Account",
+      editProfile = "Edit Profile",
+      notification = "Notification",
+      general = "General",
+      settings = "Settings",
+      security = "Security",
+      privacyPolicy = "Privacy Policy",
+      logOut = "Log Out",
+      //!Bazar
+      bazar = "Bazar",
+      historyBooks = "History Books",
+      historicalSouvenirs = "Historical Souvenirs",
+      //!Cart
+      myCart = "My Cart",
+      totalPayment = "Total Payment",
+      checkoutNow = "Checkout Now",
+      //!Payment Strings
+      checkoutScreen = "Checkout Screen",
+      deliveryAddress = "Delivery Address",
+      change = "Change",
+      items = "1 items",
+      selectedProduct = "Selected Product",
+      paymentMethod = "Payment Method",
+      confirmPayment = "Confirm Payment",
+      orderSuccess = "Order Success",
+      thankYou =
+          "Thank you for your order here and your package will be sent to your address very quickly and fast good product",
+      backToHome = "Back To Home",
+      //!Forgot password
+      forgotPasswordPage = "Forgot Password",
+      sendVerificationCode = "Send Verification Code",
+      verificationNow = "Verification Now",
+      resendCode = "Resend Code",
+      verifyAccount = "Verify Account",
+      enter4DigitCodeWeHaveSentTo = "Enter 4 digit code we have sent to ",
+      haventReceivedVerificationCode = "Haven’t received verification code?";
+}
